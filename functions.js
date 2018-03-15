71f5c24cb00c47fc0a725a1f61f8c12938610d07
Edward M <edward@bigewiki.com>
2018-03-14 20:58:08 -0700

    figured out avg age for queued projects and formatted footer tables (they are pretty)

--- functions.js
+++ functions.js
@@ -112,8 +112,11 @@ total_waiting_for_customer=0;
 total_new=0;
 // setting agent review queue
 total_agent_review=0;
-// setting total new age
+// setting ages
 total_new_age=0;
+total_cxreview_age=0;
+total_waiting_age=0;
+total_agentreview_age=0;
 /************************************/
 // Looping through all the projects gathered through the API
 /************************************/
@@ -129,7 +132,14 @@ for (var i=0; i<jsObject.service_list.length; i++) {
   if(jsObject.service_list[i].status=="Customer Review"){
     //count up total_customer_review
     total_customer_review=total_customer_review+1;
+    //update display for total_customer_review
     $('#total_customer_review').html(total_customer_review);
+    //total up the amount for total_cxreview_age (all projects combined)
+    total_cxreview_age=total_cxreview_age+jsObject.service_list[i].age;
+    //calculate and round average age for customer review
+    cxreview_avg_age=precisionRound(total_cxreview_age/total_customer_review, 1);
+    //update cxreview_avg_age display
+    $('#cxreview_avg_age').html(cxreview_avg_age);
     // check if it's been within 7 days
     if(jsObject.service_list[i].age < 8){
       total_cxreview_lastweek=total_cxreview_lastweek+1;
@@ -140,22 +150,40 @@ for (var i=0; i<jsObject.service_list.length; i++) {
   if(jsObject.service_list[i].status=="Waiting For Cust"){
     //count up total_waiting_for_customer
     total_waiting_for_customer=total_waiting_for_customer+1;
+    //update display for the total in queue for waiting for customer
     $('#total_waiting_for_customer').html(total_waiting_for_customer);
+    //add up sum for all age values for all projects in this queue
+    total_waiting_age=total_waiting_age+jsObject.service_list[i].age;
+    // calculate and round the average age
+    waiting_avg_age=precisionRound(total_waiting_age/total_waiting_for_customer, 1);
+    // update the display
+    $('#waiting_avg_age').html(waiting_avg_age);
   }
 
   if(jsObject.service_list[i].status=="New"){
     //count up total_new
     total_new=total_new+1;
-    total_new_age=total_new_age+jsObject.service_list[i].age;
+    // update the display for the total in this queue
     $('#total_new').html(total_new);
+    //add up the total age for all all new projects
+    total_new_age=total_new_age+jsObject.service_list[i].age;
+    //calculate the average age and round it
     new_avg_age=precisionRound(total_new_age/total_new, 1);
+    //update the display for the average age
     $('#new_avg_age').html(new_avg_age);
   }
 
   if(jsObject.service_list[i].status=="Agent Review"){
     //count up total_agent_review
     total_agent_review=total_agent_review+1;
+    //update display for the total in this queue
     $('#total_agent_review').html(total_agent_review);
+    //add up the total age for all all agent review projects
+    total_agentreview_age=total_agentreview_age+jsObject.service_list[i].age;
+    //calculate the average age and round it
+    agentreview_avg_age=precisionRound(total_agentreview_age/total_agent_review, 1);
+    //update the display for the average age
+    $('#agentreview_avg_age').html(agentreview_avg_age);
   }
 
     if(jsObject.service_list[i].status=="Cancelled"){
